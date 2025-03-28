5427b890d991a81606c462d0ee66d647d8e1cbcf
marcelonicacio <marcelonicacio@me.com>
2025-03-28 18:32:48 +0000

    user insert, delete, update

--- src/main/java/com/nicas/resources/UserResource.java
+++ src/main/java/com/nicas/resources/UserResource.java
@@ -1,21 +1,27 @@
 package com.nicas.resources;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nicas.domain.User;
 import com.nicas.dto.UserDTO;
 import com.nicas.services.UserService;
 
 @RestController
-@RequestMapping(value ="users")
+@RequestMapping(value = "/users")
 public class UserResource {
 	
 	@Autowired
@@ -26,5 +32,33 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	@PutMapping
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto,@PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	
 	}
 }
