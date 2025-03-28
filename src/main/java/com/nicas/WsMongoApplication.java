8fc6656bcbbc05e7be863a807e590e0f8d694f2c
marcelonicacio <marcelonicacio@me.com>
2025-03-28 14:11:10 +0000

    create project

---
+++ src/main/java/com/nicas/WsMongoApplication.java
@@ -0,0 +1,13 @@
+package com.nicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicas/WsMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
